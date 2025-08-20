30eaf1c9ffe39e151bd3c889e9e157ed022f3ffe
hp <diamonfnwachi40@gmail.com>
2025-08-20 19:07:58 +0100

    first commit

---
+++ shop/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+from rest_framework.permissions import BasePermission
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+
+
+class CustomUser(AbstractUser):
+    is_admin = models.BooleanField(default=False)
+    is_customer = models.BooleanField(default=False)
+
+class IsAdminUser(BasePermission):
+    def has_permission(self, request, view):
+        return request.user and request.user.is_authenticated and request.user.is_admin
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    price = models.PositiveIntegerField(default=0)
+    stock = models.PositiveIntegerField(default=0)
+    category = models.CharField(max_length=2557)
+    image = models.ImageField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Cart(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=0)
+
+class Order(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    total_amount = models.PositiveIntegerField(default=0)
+    status = models.CharField(default='Pending')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=0)
+    price = models.PositiveIntegerField(default=0)
+
+class Payment(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    transaction_id = models.CharField(max_length=255)
+    payment_status = models.CharField(default='Pending')
+    method = models.CharField(max_length=255)
